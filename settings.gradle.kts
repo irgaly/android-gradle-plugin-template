24877fd37dd364f477830d93d0da189a5f858d64
irgaly <irgaly@gmail.com>
2022-01-24 23:51:21 +0900

    use plugin version infix style

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,12 +7,12 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application").version("7.0.1")
-        id("com.android.library").version("7.0.1")
-        kotlin("android").version("1.6.10")
+        id("com.android.application") version "7.0.1"
+        id("com.android.library") version "7.0.1"
+        kotlin("android") version "1.6.10"
         // if you want to use your plugin from Maven Plugin Portal, specify it's version here
         // and detach :plugin composite by removing `includeBuild("plugin")`
-        //id("org.sample.plugin").version("0.1.0")
+        //id("org.sample.plugin") version "0.1.0"
     }
 }
 dependencyResolutionManagement {
