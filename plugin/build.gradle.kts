440abf074674a4493fa93dd3f883c1358b3b0c11
irgaly <irgaly@gmail.com>
2023-04-09 17:53:23 +0900

    sign plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,15 +1,11 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.publish)
+    signing
 }
 
 group = "org.sample.plugin"
 version = libs.versions.sampleplugin.get()
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
 
 gradlePlugin {
     website.set("https://github.com/example/example")
@@ -24,3 +20,28 @@ gradlePlugin {
         }
     }
 }
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+signing {
+    useInMemoryPgpKeys(
+        providers.gradleProperty("signingKey").orNull,
+        providers.gradleProperty("signingPassword").orNull
+    )
+}
+
+/*
+// For GItHub Actions CI signing
+if (providers.environmentVariable("CI").isPresent) {
+    apply(plugin = "signing")
+    extensions.configure<SigningExtension> {
+        useInMemoryPgpKeys(
+            providers.environmentVariable("SIGNING_PGP_KEY").orNull,
+            providers.environmentVariable("SIGNING_PGP_PASSWORD").orNull
+        )
+    }
+}
+ */
