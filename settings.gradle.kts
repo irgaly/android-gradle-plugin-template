3ad639e9226102da6c02ca2e0ebe6fa8385e7b90
irgaly <irgaly@gmail.com>
2023-06-12 21:11:41 +0900

    add foojay-esolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,9 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.5.0")
+}
 rootProject.name = "android-gradle-plugin-template"
 include(":sample")
 // if you want to use your plugin from Maven Plugin Portal, comment out below `includeBuild("plugin")` line.
