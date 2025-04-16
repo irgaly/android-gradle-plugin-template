0b762dce52f8dd1d51ca1c65067ae17ab0ae61af
irgaly <irgaly@gmail.com>
2025-04-16 13:36:58 +0900

    remove: Gradle afterEvaluate

--- plugin/src/main/kotlin/org/sample/GreetingPlugin.kt
+++ plugin/src/main/kotlin/org/sample/GreetingPlugin.kt
@@ -7,10 +7,8 @@ class GreetingPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         val extension = target.extensions.create("greeting", GreetingExtension::class.java)
         val task = target.tasks.register("greeting", GreetingTask::class.java)
-        target.afterEvaluate {
-            task.configure { target ->
-                target.who.set(extension.who)
-            }
+        task.configure { target ->
+            target.who.set(extension.who)
         }
     }
 }
