01a1ee292c79ad1a1d4e1e7837eaf00ecb2e94c7
irgaly <irgaly@gmail.com>
2025-04-16 13:57:24 +0900

    Sample: use enableEdgeToEdge()

--- sample/src/main/kotlin/org/sample/app/MainActivity.kt
+++ sample/src/main/kotlin/org/sample/app/MainActivity.kt
@@ -2,6 +2,7 @@ package org.sample.app
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,9 +18,7 @@ import androidx.core.view.WindowInsetsControllerCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        window.statusBarColor = Color.Transparent.toArgb()
-        window.navigationBarColor = Color.Transparent.toArgb()
+        enableEdgeToEdge()
         WindowInsetsControllerCompat(
             window,
             findViewById(android.R.id.content)
