df03ef6b16087e7789aaed3d9dfcfa50d385cc28
irgaly <irgaly@gmail.com>
2025-10-12 14:28:55 +0900

    update sample

--- sample/src/main/kotlin/org/sample/app/MainActivity.kt
+++ sample/src/main/kotlin/org/sample/app/MainActivity.kt
@@ -1,28 +1,22 @@
 package org.sample.app
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsControllerCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        WindowInsetsControllerCompat(
-            window,
-            findViewById(android.R.id.content)
-        ).isAppearanceLightStatusBars = true
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
         setContent {
             MaterialTheme {
                 Box(
